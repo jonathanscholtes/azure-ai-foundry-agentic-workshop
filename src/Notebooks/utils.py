157dce49e610d13c632987e17c10f90ebac3a1a2
Jonathan Scholtes <jscholtes128@gmail.com>
2025-04-24 18:31:10 -0500

    added evals

--- src/Notebooks/utils.py
+++ src/Notebooks/utils.py
@@ -22,6 +22,33 @@ def pretty_print_messages(update):
         print("\n")
 
 
+def extract_graph_response(query, graph):
+    """
+    Function to stream the graph and extract the final human response.
+    
+    Args:
+    - query: The user query to send to the graph.
+    - graph: The graph object to stream from.
+    
+    Returns:
+    - final_response: The content of the last human message if found, otherwise None.
+    """
+    final_response = None  
+    
+    # Start streaming the graph with the provided query
+    for step in graph.stream(
+        {"messages": [{"role": "user", "content": query}]}
+    ):
+
+        for key in step:
+            if step[key] and 'messages' in step[key]:
+                human_messages = [msg for msg in step[key]['messages'] if isinstance(msg, HumanMessage)]
+                if human_messages:
+                    final_response = human_messages[-1].content
+
+    return final_response
+
+
 def pretty_print_response(conversation):
     for message in conversation['messages']:
         if isinstance(message, HumanMessage):
